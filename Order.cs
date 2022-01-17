2f605a1fc34ea834670e01cc0eee1799cedab285
Alethea Chan <s10221902@connect.np.edu.sg>
2022-01-17 13:13:07 +0800

    read&display cinema+unfinished order class

---
+++ Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    class Order
+    {
+        private int orderno;
+
+        public int orderNo
+        {
+            get { return orderno; }
+            set { orderno = value; }
+        }
+        private DateTime orderdateTime;
+
+        public DateTime orderDateTime
+        {
+            get { return orderdateTime; }
+            set { orderdateTime = value; }
+        }
+        private double amount;
+
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace PRG2_Assignment
 {
@@ -6,7 +8,31 @@ namespace PRG2_Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Cinema> cList = new List<Cinema>();
+            List<Movie> mList = new List<Movie>();
+            List<Screening> sList = new List<Screening>();
+            ReadCinema(cList);
+            DisplayCinema(cList);
+        }
+
+
+        // ------------------- Load Cinema Data -------------------
+        static void ReadCinema(List<Cinema> cList)
+        {
+            string[] data = File.ReadAllLines("Cinema.csv");
+            for (int i = 1; i < data.Length; i++)
+            {
+                string[] values = data[i].Split(",");
+                cList.Add(new Cinema(values[0], Convert.ToInt32(values[1]), Convert.ToInt32(values[2])));
+            }
+        }
+        static void DisplayCinema(List<Cinema> cList)
+        {
+            Console.WriteLine("{0,-18}{1,-15}{2,-10}", "Name", "Hall Number", "Capacity");
+            foreach (Cinema c in cList)
+            {
+                Console.WriteLine("{0,-18}{1,-15}{2,-10}", c.Name, c.HallNo, c.Capacity);
+            }
         }
     }
 }
