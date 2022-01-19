becea5906a6aaf1f26e6b8a1a1d1ceb6092c9c60
Shi Wei Chew <s10221849@connect.np.edu.sg>
2022-01-19 15:35:04 +0800

    hi

--- Screening.cs
+++ Screening.cs
@@ -8,5 +8,37 @@ namespace PRG2_Assignment
 {
     class Screening
     {
+        private int screeningno;
+        public int ScreeningNo { get; set; }
+
+        private DateTime screeningDateTime;
+        public DateTime ScreeningDateTime { get; set; }
+        
+        private string screeningType;
+        public string ScreeningType { get; set; }
+
+        private int seatsRemaining; 
+        public int SeatsRemaining { get; set; }
+
+        private Cinema cinema;
+        public Cinema Cinema { get; set; }
+
+        private Movie movie;
+        public Movie Movie { get; set; }
+
+        public Screening() { }
+        public Screening(int sno, DateTime sdtime, string stype, Cinema c, Movie m)
+        {
+            ScreeningNo = sno;
+            ScreeningDateTime = sdtime;
+            ScreeningType = stype;
+            Cinema = c;
+            Movie = m;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
