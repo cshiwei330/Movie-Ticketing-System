a824384e918dcb1c47857dcf2aad191b4764063f
Alethea Chan <s10221902@connect.np.edu.sg>
2022-01-20 03:06:52 +0800

    YAY

--- Program.cs
+++ Program.cs
@@ -13,8 +13,9 @@ namespace PRG2_Assignment
             List<Cinema> cList = new List<Cinema>();
             List<Movie> mList = new List<Movie>();
             List<Screening> sList = new List<Screening>();
-            //ReadCinema(cList);
-            //DisplayCinema(cList);
+            ReadCinema(cList);
+            DisplayCinema(cList);
+            Console.WriteLine("\n");
             ReadMovie(mList);
             DisplayMovie(mList);
             //ReadScreening(sList, cList, mList);
@@ -50,7 +51,7 @@ namespace PRG2_Assignment
                 string[] mvalues = mdata[i].Split(",");
                 string genreGiven = mvalues[2]; //store the genre given in csv
 
-                static List<string> generateGenre (string genreGiven) //method to return genre list for movies obj
+                static List<string> generateGenre(string genreGiven) //method to return genre list for movies obj
                 {
                     List<string> genrelist = new List<string>(); //create a new string everytime 
                     string slash = "/";
@@ -80,66 +81,66 @@ namespace PRG2_Assignment
         }
         static void DisplayMovie(List<Movie> mList)  //to be deleted
         {
-            Console.WriteLine("{0,-40}{1,-7}{2,40}{3,-8}{4,-12}", "Title", "Duration (mins)", "Genre", "Classification", "Opening Date");
+            Console.WriteLine("{0,-35}{1,-23}{2,-20}{3,-27}{4,-25}", "Title", "Duration (mins)", "Classification", "Opening Date", "Genre");
             foreach (Movie m in mList)
             {
-                Console.WriteLine("{0,-40}{1,-7}{2,40}{3,-8}{4,-12}", m.Title, m.Duration, m.Classification, m.OpeningDate, m.genreList);
+                Console.WriteLine("{0,-35}{1,-23}{2,-20}{3,-27}{4,-25}", m.Title, m.Duration, m.Classification, m.OpeningDate, m.genreList);
             }
         }
 
         // ------------------- Load Screening Data -------------------
-        static void ReadScreening(List<Screening> sList, List<Cinema> cList, List<Movie> mList)
-        {
-            string[] sdata = File.ReadAllLines("Screening.csv");
-            for (int i = 1; i < sdata.Length; i++)
-            {
-                string[] svalues = sdata[i].Split(",");
-                string cinemaName = svalues[2];
-                static Cinema CinemaSearch(List<Cinema> cList, string cinemaName)
-                {
-                    for (int i = 0; i < cList.Count; i++)
-                    {
-                        Cinema c = cList[i];
-                        if (cinemaName == c.Name)
-                        {
-                            return c;
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                    return null;
-                }
-                Cinema result = CinemaSearch(cList, cinemaName);
+        //static void ReadScreening(List<Screening> sList, List<Cinema> cList, List<Movie> mList)
+        //{
+        //    string[] sdata = File.ReadAllLines("Screening.csv");
+        //    for (int i = 1; i < sdata.Length; i++)
+        //    {
+        //        string[] svalues = sdata[i].Split(",");
+        //        string cinemaName = svalues[2];
+        //        static Cinema CinemaSearch(List<Cinema> cList, string cinemaName)
+        //        {
+        //            for (int i = 0; i < cList.Count; i++)
+        //            {
+        //                Cinema c = cList[i];
+        //                if (cinemaName == c.Name)
+        //                {
+        //                    return c;
+        //                }
+        //                else
+        //                {
+        //                    continue;
+        //                }
+        //            }
+        //            return null;
+        //        }
+        //        Cinema result = CinemaSearch(cList, cinemaName);
 
-                string movieName = svalues[3];
-                static Movie MovieSearch(List<Movie> mList, string movieName)
-                {
-                    for (int a = 0; a < mList.Count; a++)
-                    {
-                        Movie m = mList[a];
-                        if (movieName == m.Title)
-                        {
-                            return m;
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                    return null;
-                }
-                Movie result2 = MovieSearch(mList, movieName);
-                sList.Add(new Screening(ScreeningNo, Convert.ToDateTime(svalues[0]), svalues[1], result,result2));
-            }
-        }
-        static void DisplayScreening(List<Screening> sList)  //to be deleted
-        {
-            foreach (Screening s in sList)
-            {
-                Console.WriteLine("{0,-30}{1,-20}{2,-6}{3,-30}{4,-40}", s.ScreeningNo, s.ScreeningDateTime, s.ScreeningType, s.Cinema.Name, s.Movie.Title);
-            }
-        }
+        //        string movieName = svalues[3];
+        //        static Movie MovieSearch(List<Movie> mList, string movieName)
+        //        {
+        //            for (int a = 0; a < mList.Count; a++)
+        //            {
+        //                Movie m = mList[a];
+        //                if (movieName == m.Title)
+        //                {
+        //                    return m;
+        //                }
+        //                else
+        //                {
+        //                    continue;
+        //                }
+        //            }
+        //            return null;
+        //        }
+        //        Movie result2 = MovieSearch(mList, movieName);
+        //        sList.Add(new Screening(ScreeningNo, Convert.ToDateTime(svalues[0]), svalues[1], result,result2));
+        //    }
+        //}
+        //static void DisplayScreening(List<Screening> sList)  //to be deleted
+        //{
+        //    foreach (Screening s in sList)
+        //    {
+        //        Console.WriteLine("{0,-30}{1,-20}{2,-6}{3,-30}{4,-40}", s.ScreeningNo, s.ScreeningDateTime, s.ScreeningType, s.Cinema.Name, s.Movie.Title);
+        //    }
+        //}
     }
 }
